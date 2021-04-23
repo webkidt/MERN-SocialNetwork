4e8dec9c8b52d5264f3c6e7d7916e721745da8ca
David Pam <niratpam@gmail.com>
2021-04-23 23:09:58 +0100

    Added Card Component to Home Page

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,8 +1,37 @@
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import unicornbikeImg from '../assets/images/unicornbike.jpg';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing(5),
+  },
+  title: {
+    padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
+    color: theme.palette.openTitle,
+  },
+  media: {
+    minHeight: 400,
+  }
+}));
+
 const Home = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Hello World From Express!</h1>
-    </div>
+    <Card className={classes.card}>
+      <Typography variant='h6' className={classes.title}>
+        Home Page
+      </Typography>
+      <CardMedia className={classes.media} image={unicornbikeImg} title='Unicorn Bicycle' />
+      <CardContent>
+        <Typography variant='body2' component='p'>
+          Welcome to MERN Skeleton home page.
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
